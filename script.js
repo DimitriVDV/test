cae3f4b34a828d1052d57dc628ba304a672b7e45
DimitriVDV <dimitri.vdvpro@gmail.com>
2025-12-10 09:09:14 +0100

    first

---
+++ script.js
@@ -0,0 +1,69 @@
+/*let nom;
+let prenom;
+nom = prompt("saisissez votre nom");
+prenom = prompt("saisissez votre prénom");
+alert(nom);
+alert(prenom);
+let nbreA;
+let nbreB;
+let resultat;
+nbreA = prompt("nbre1");
+nbreB = prompt("nbre2");
+resultat = nbreA*nbreB;
+alert(resultat);
+let c;
+let f;
+c=prompt("saisir degré a convertir");
+f=(c*9/5)+32;
+alert(f);
+let a="100";
+let b=100;
+let c=1;
+let d=true;
+alert("ceci blala:"+a);
+b--;
+alert("ceci blala:"+b);
+c += a;
+alert("ceci blala:"+c);
+d=!d;
+alert("ceci blala:"+d);
+let nbre=prompt("saisir le numéro de la daronne de samir");
+nbre=nbre%2;
+if(nbre > 0){
+    alert("nombre impair");
+}
+else{
+    alert("nombre pair");
+}
+const année=2025;
+let age=prompt("saisir année de naissance");
+let valid=année-age;
+if (valid<18) {
+    alert("mineur");
+} else {
+    alert("majeur");
+}
+let nbrea=prompt("saisir a");
+let ope=prompt("saisir une opération");
+let nbreb=prompt("saisir b");
+switch (ope) {
+    case "+":
+        ope= +nbrea + +nbreb;
+        alert(ope);
+        break;
+    case "/":
+        ope=+nbrea / +nbreb;
+        alert(ope);
+        break;
+    case "*":
+        ope=+nbrea * +nbreb;
+        alert(ope);
+        break;
+    case "-":
+        ope=+nbrea - +nbreb;
+        alert(ope);
+        break;        
+    default:
+        alert("entrez un opérateur valide");
+        break;
+}*/
